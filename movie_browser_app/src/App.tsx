ae7ac6fd1e8b6fed585542ce0f3c71e07f6618d1
babita0508 <143036774+babita0508@users.noreply.github.com>
2023-08-24 13:03:06 +0530

    added build file

--- movie_browser_app/src/App.tsx
+++ movie_browser_app/src/App.tsx
@@ -2,13 +2,13 @@ import "./App.css";
 import ListPage from "./component/ListPage";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import MovieDetailsPage from "./component/MovieDetailsPage";
+import ErrorBoundary from "./ErrorBoundary"; // Import the ErrorBoundary component
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ListPage />} />
-        <Route path="/details/:id" element={<MovieDetailsPage />} />{" "}
-        {/* Use the imported component */}
+        <Route path="/" element={<ErrorBoundary><ListPage /></ErrorBoundary>} />
+        <Route path="/details/:id" element={<ErrorBoundary><MovieDetailsPage /></ErrorBoundary>} />
       </Routes>
     </Router>
   );
